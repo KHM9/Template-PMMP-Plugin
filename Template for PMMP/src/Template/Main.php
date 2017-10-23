0da8336f69c54cec7958c860042b021d5fc1630e
MazaiCrafty <takatim1574@icloud.com>
2017-10-23 21:39:07 +0900

    Add files via upload

---
+++ Template for PMMP/src/Template/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+#%{Namespace}
+
+# Base
+use pocketmine\plugin\PluginBase;
+
+# Command
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+		$this->getLogger()->info("Hello World!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "example":
+				$sender->sendMessage("Example command output");
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public function onDisable(){
+		$this->getLogger()->info("Bye!");
+	}
+}
